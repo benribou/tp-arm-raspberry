94d58d6b606cfa9380eb561301ae8cf00750f09c
benribou <ribou.benjamin@gmail.com>
2025-02-25 17:04:51 +0100

    Add targetTemp to stm32

--- src/lib/serialPort.ts
+++ src/lib/serialPort.ts
@@ -14,6 +14,7 @@ const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
 
 parser.on('data', (line: string) => {
   try {
+    console.log(line)
     const data = JSON.parse(line.trim());
     lastReading = { temperature: data.temperature, humidity: data.humidity };
     console.log('Nouvelle lecture depuis STM32 :', lastReading);
